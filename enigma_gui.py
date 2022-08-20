5203ecd4f5ecc6ffa4ac0d6384bf7d9a366e6b9f
Stier56 <46590374+Stier56@users.noreply.github.com>
2022-08-21 00:42:34 +0900

    fix: Modified to allow GUI operation.

---
+++ enigma_gui.py
@@ -0,0 +1,53 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.properties import StringProperty, ListProperty
+from enigma_gui_component import encryption
+
+
+class EnigmaScreen(BoxLayout):
+    slot1_scrambler_number = StringProperty('1')
+    slot2_scrambler_number = StringProperty('1')
+    slot3_scrambler_number = StringProperty('1')
+
+    slot1_initial_position = StringProperty('a')
+    slot2_initial_position = StringProperty('a')
+    slot3_initial_position = StringProperty('a')
+
+    plugboard_list = ListProperty([chr(num) for num in range(97, 123)])
+
+    input_string = StringProperty()
+    output_string = StringProperty()
+    
+    def __init__(self, **kwargs):
+        super(EnigmaScreen, self).__init__(**kwargs)
+
+    def change_plugboard(self, index, new_alphabet):
+        new_alphabet_index = self.plugboard_list.index(new_alphabet)
+        tmp = self.plugboard_list[index]
+        self.plugboard_list[index] = new_alphabet
+        self.plugboard_list[new_alphabet_index] = tmp
+
+    def run_encryption(self): 
+        self.output_string = encryption(self.input_string,
+                                        self.slot1_scrambler_number, 
+                                        self.slot2_scrambler_number, 
+                                        self.slot3_scrambler_number, 
+                                        self.slot1_initial_position, 
+                                        self.slot2_initial_position, 
+                                        self.slot3_initial_position,
+                                        self.plugboard_list)
+
+Builder.load_file('./enigma_gui.kv')
+
+class EnigmaApp(App):
+    def __init__(self, **kwargs):
+        super(EnigmaApp, self).__init__(**kwargs)
+        self.title = 'Enigma Simulator'    
+
+    def build(self):
+        return EnigmaScreen()
+
+
+if __name__ == '__main__':
+    EnigmaApp().run()
--- enigma_gui_component.py
+++ enigma_gui_component.py
@@ -1,24 +1,22 @@
 class scrambler:
     def __init__(self, scrambler_number):
-        self.scrambler_number = scrambler_number
-        self.scrambler_slot = []
-
-        if self.scrambler_number == 1: 
+        if scrambler_number == 1: 
             self.wiring_R = [10, 21, 5, 20, 7, 2, 23, 18, 1, 11, 3, 0, 6, 24, 25, 16, 14, 17, 13, 22, 4, 15, 8, 12, 19, 9]
             self.wiring_L = [24, 8, 7, 6, 21, 18, 9, 22, 3, 11, 2, 23, 12, 0, 4, 16, 14, 20, 17, 15, 19, 25, 5, 13, 10, 1]
-        elif self.scrambler_number == 2:
+        elif scrambler_number == 2:
             self.wiring_R = [7, 6, 22, 2, 0, 20, 8, 15, 10, 5, 24, 9, 1, 21, 13, 19, 16, 18, 3, 14, 12, 23, 11, 4, 25, 17]
             self.wiring_L = [5, 10, 11, 13, 3, 21, 19, 1, 6, 15, 9, 18, 8, 4, 24, 7, 17, 12, 20, 14, 23, 0, 25, 22, 2, 16]
-        elif self.scrambler_number == 3:
+        elif scrambler_number == 3:
             self.wiring_R = [5, 15, 1, 6, 19, 22, 3, 16, 10, 21, 2, 25, 18, 0, 8, 13, 17, 11, 12, 4, 20, 9, 24, 23, 14, 7]
             self.wiring_L = [14, 24, 15, 4, 10, 21, 22, 20, 5, 13, 7, 8, 17, 9, 12, 16, 25, 6, 19, 0, 2, 11, 1, 23, 3, 18]
-        elif self.scrambler_number == 4:
+        elif scrambler_number == 4:
             self.wiring_R = [22, 12, 5, 17, 0, 24, 11, 19, 8, 6, 9, 4, 14, 20, 10, 23, 16, 18, 25, 7, 15, 3, 1, 2, 21, 13]
             self.wiring_L = [23, 7, 18, 5, 3, 21, 6, 13, 22, 14, 1, 24, 20, 0, 17, 8, 10, 25, 19, 11, 12, 4, 2, 16, 9, 15]
         else:
             self.wiring_R = [20, 24, 0, 3, 16, 10, 11, 15, 8, 7, 12, 5, 6, 1, 18, 4, 2, 17, 25, 21, 9, 13, 23, 19, 22, 14]
             self.wiring_L = [25, 14, 8, 22, 13, 20, 6, 15, 23, 10, 3, 21, 4, 2, 5, 1 ,17, 18, 0, 11, 9, 16, 24, 12, 19, 7]
 
+    # Set the initial position of the scrambler
     def set_scrambler_position(self, scrambler_position):
         self.scrambler_position = scrambler_position
 
@@ -40,21 +38,18 @@ class scrambler:
         self.wiring_L[0:25] = self.wiring_L[1:26]
         self.wiring_L[25] = tmp_L
 
+    # output from right to left
     def output_RtoL(self, input):
         index = self.wiring_L.index(self.wiring_R[input])
         return index
-
+    # output from left to right
     def output_LtoR(self, input):
         index = self.wiring_R.index(self.wiring_L[input])
         return index
 
 class plugbord:
-    def __init__(self):
-        self.char_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-    def change_list(self, char1, char2):
-        self.char_list[self.char_list.index(char1)] = char2
-        self.char_list[self.char_list.index(char2)] = char1
+    def __init__(self, new_list):
+        self.char_list = new_list
 
     def input(self, input_char):
         index = self.char_list.index(input_char)
@@ -70,47 +65,41 @@ def reflector(input):
 
     return int(output)
 
+# Check input string. Allow lowercase letters only.
+def input_string_check(input_char):
+    if ord(input_char) < 97 or ord(input_char) > 122:
+        output_char = ' '
+    else:
+        output_char = input_char
 
-def main():
-    input_val = input('スロット1にセットするスクランブラの番号(1~5)を選択してください。\n')
-    scrambler1 = scrambler(int(input_val))
-    scrambler1.scrambler_slot = 1
-    input_val = input('スロット2にセットするスクランブラの番号(1~5)を選択してください。\n')
-    scrambler2 = scrambler(int(input_val))
-    scrambler2.scrambler_slot = 2
-    input_val = input('スロット3にセットするスクランブラの番号(1~5)を選択してください。\n')
-    scrambler3 = scrambler(int(input_val))
-    scrambler3.scrambler_slot = 3
+    return output_char
 
-    input_val = input('スクランブラ1の初期位置(a~z)を選択してください。\n')
-    scrambler1.set_scrambler_position(ord(input_val) - 97)
-    input_val = input('スクランブラ２の初期位置(a~z)を選択してください。\n')
-    scrambler2.set_scrambler_position(ord(input_val) - 97)
-    input_val = input('スクランブラ３の初期位置(a~z)を選択してください。\n')
-    scrambler3.set_scrambler_position(ord(input_val) - 97)
+def encryption(input_string, slot1_number, slot2_number, slot3_number, slot1_position, slot2_position, slot3_position, plugboard_list):
+    # Set scrambler for each slot
+    scrambler1 = scrambler(int(slot1_number))
+    scrambler2 = scrambler(int(slot2_number))
+    scrambler3 = scrambler(int(slot3_number))
 
-    plugbord_input = plugbord()
+    # Set the initial position of the scrambler
+    scrambler1.set_scrambler_position(ord(slot1_position) - 97)
+    scrambler2.set_scrambler_position(ord(slot2_position) - 97)
+    scrambler3.set_scrambler_position(ord(slot3_position) - 97)
 
-    while True:
-        change_char = input('プラグボードで入れ替える文字を小文字で入力してください。\nex) aとbを入れ替える場合  ab\n 入力をやめる場合は N を入力してください。\n')
-
-        if change_char[0] == 'N':
-            break
-
-        plugbord_input.change_list(change_char[0], change_char[1])
+    # Create an instance of the plugboard
+    using_plugbord = plugbord(plugboard_list)
 
     scrambler1_count = 0
     scrambler2_count = 0
 
-    print('暗号化するアルファベット(小文字)を入力してください。やめる場合は N を入力してください\n')
+    input_string_list = list(input_string)
+    output = []
+    for input_char in input_string_list:
+        input_char = input_string_check(input_char)
+        if input_char == ' ':
+            output.append(' ')
+            continue
 
-    while True:
-        input_char = input('input: ')
-
-        if input_char == 'N':
-            break
-
-        input_char_index = plugbord_input.input(input_char)
+        input_char_index = using_plugbord.input(input_char)
 
         out = scrambler1.output_RtoL(input_char_index)
         out = (out + (scrambler2.scrambler_position - scrambler1.scrambler_position)) % 26
@@ -139,10 +128,11 @@ def main():
         scrambler1.scrambler_rotation()
         scrambler1_count = scrambler1_count + 1
 
-        print('output： ' + plugbord_input.output(out))
+        output.append(using_plugbord.output(out))
+
+    output_string = "".join(output)
+    return output_string
 
 
-if __name__ == '__main__':
-    main()
 
 
